4ce1c4249ea16a74a960c2a2b748e9541249eaae
Gustavo Duarte <gustavoduarte1999@hotmail.com>
2020-02-26 03:43:09 -0300

    Em construção da funcão Buscar Aluno

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const fs = require('fs')
 const funcoes = require("./funcoes")
 var alunosDaEscola = require("./database/alunosDaEscola.json")
 
- 
-console.log(alunosDaEscola)
+funcoes.buscarAluno("Guilherme")
+//console.log(alunosDaEscola)
+
 
 
 fs.writeFileSync("./database/alunosDaEscola.json",JSON.stringify(alunosDaEscola))
