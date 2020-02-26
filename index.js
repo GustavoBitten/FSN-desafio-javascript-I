42bd82ee3c91cd80a6737a0ce06c36bd945244c9
Gustavo Duarte <gustavoduarte1999@hotmail.com>
2020-02-26 02:08:40 -0300

    estrutura para o sistema

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// Base a ser utilizada
-const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
+const fs = require('fs')
+
+const funcoes = require("./funcoes")
+var alunosDaEscola = require("./database/alunosDaEscola.json")
+
+ 
+console.log(alunosDaEscola)
 
 
-// implementação
+fs.writeFileSync("./database/alunosDaEscola.json",JSON.stringify(alunosDaEscola))
